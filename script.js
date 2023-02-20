8767ad8766426247b061f94e5ac44246f402feac
shivani sawarkar <117519427+shivanisawarakar@users.noreply.github.com>
2023-02-20 15:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
-
+let btn=document.getElementById("addTodoBtn")
+btn.addEventListener("click",funcall)
+function funcall(){
+	if(document.getElementById("newTodoInput").value!=""){
+	    let store=document.createElement("li")
+	    store.innerText=document.getElementById("newTodoInput").value
+        todoList.appendChild(store)
+	}
+    else alert("write something")
+	document.getElementById("newTodoInput").value=""
+}
